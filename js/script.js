7e678f2a8f2e74d886e1cba60bb4f872c727a7a6
F1NQB3R <mtam1222@gmail.com>
2018-09-15 15:23:27 -0400

    Test Firebase Auth

---
+++ js/script.js
@@ -0,0 +1,58 @@
+(function (){
+  // Initialize Firebase
+  const config = {
+      apiKey: "AIzaSyAYS_mXkzOluLBciiyPyJk092rc_-eCMvg",
+      authDomain: "hackthenorth-16b34.firebaseapp.com",
+      databaseURL: "https://hackthenorth-16b34.firebaseio.com",
+      projectId: "hackthenorth-16b34",
+      storageBucket: "hackthenorth-16b34.appspot.com",
+      messagingSenderId: "1089172554056"
+  };
+  firebase.initializeApp(config);
+
+  //Get Elements
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const btnLongin = document.getElementById('btnLogin');
+  const btnSignUp = document.getElementById('btnSignUp');
+  const btnLogout = document.getElementById('btnLogout');
+
+  //Add login event
+  btnLogin.addeventListener('click', e => {
+    //Get email / pass
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    //Sign in
+    const promise = auth.signInWithEmailAndPassword(email,password);
+    promise.catch(e=>console.log(e.message));
+  });
+
+  btnSignUp.addeventListener("click, e=> "{
+    //Get email / pass
+    //TODO: CHECK 4 REAL NUMBER
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    //Sign in
+    const promise = auth.createUserWithEmailAndPassword(email,password);
+    promise.catch(e=>console.log(e.message));
+  });
+
+  btnLogout.addeventListener('click', e=>{
+    firebase.auth().signOut();
+  });
+
+  //Realtime Listener
+  firebase.auth().onAuthStateChanged(firebaseuser => {
+    if (firebaseuser){
+      console.log(firebaseUser);
+      btnLogout.classList.remove('hide');
+    }else{
+      console.log('not logged in');
+      btnLogout.classList.add('hide');
+    }
+  });
+
+
+}());
